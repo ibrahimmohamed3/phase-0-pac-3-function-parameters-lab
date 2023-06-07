7c258a166a282e70de70170c4fb532271556f8d7
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-08 00:13:52 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+// function introduction (name){
+//     console.log(`"Hi, my name is ${name} `)
+// }
+function introductionWithLanguageOptional(name, language){
+ console.log("Hi, my name is [name] and i am learning program in [language]")
+}
